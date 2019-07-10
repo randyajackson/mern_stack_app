4c23c74310291ec4e4830020bedd6d1dc79b2bc5
randyajackson <rajackson.dev@gmail.com>
2019-07-09 19:24:30 -0600

    7/9/19 7:30p

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -1,13 +1,25 @@
 const express = require ('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-require('dontenv').config();
+
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect('mongodb://localhost/playground', {useNewUrlParser: true, useCreateIndex: true})//connect creates a promise
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect', err));
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
 
 app.listen(port, () => {
     console.log('Server is running on port:', {port});
