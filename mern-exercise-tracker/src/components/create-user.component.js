86be91994b1a834f582e4f4ba9dbd7a0a07608d4
randyajackson <rajackson.dev@gmail.com>
2019-07-16 20:16:47 -0600

    7/16/19 8:20p

--- mern-exercise-tracker/src/components/create-user.component.js
+++ mern-exercise-tracker/src/components/create-user.component.js
@@ -2,10 +2,61 @@ import React, { Component } from 'react';
 
 export default class CreateUser extends Component {
 
+    constructor(props){
+        super(props);
+
+        //binding this to the CreateExercise class
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: ''
+        }
+    }
+    //state creates variables in react
+
+    onChangeUsername(e){
+        //this.state.username = 'Randy' you don't want to change state this way
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onSubmit(e){
+       
+        e.preventDefault(); // prevents normal html form
+        const user = {
+            username: this.state.username,
+
+        }
+
+        console.log(user);
+
+        this.setState({
+            username: ''
+        })
+            
+        
+    }
+
     render() {
         return (
             <div>
-                <p>Create User component</p>
+                <h3>Create New User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         );
     }
