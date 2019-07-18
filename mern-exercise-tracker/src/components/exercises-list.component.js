82daee38e8d28bf92b8bf9e1991421d6a6919696
randyajackson <rajackson.dev@gmail.com>
2019-07-17 20:09:37 -0600

    7/17/19 8:10p

--- mern-exercise-tracker/src/components/exercises-list.component.js
+++ mern-exercise-tracker/src/components/exercises-list.component.js
@@ -1,11 +1,77 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Exercise = props => (
+    <tr>
+        <td>{props.exercise.username}</td>
+        <td>{props.exercise.description}</td>
+        <td>{props.exercise.duration}</td>
+        <td>{props.exercise.date.substring(0,10)}</td>
+        <td>
+            <Link to ={"/edit/" + props.exercise._id}> edit </Link> | <a href="#" onClick = { () => {props.deleteExercise(props.exercise._id)}}>delete</a>
+        </td>
+    </tr>
+)
+
+//exercise is implemented as a function component, ExercisesList is a class component
 
 export default class ExercisesList extends Component {
+
+    constructor(props){
+        super(props);
+
+        //binding this to the CreateExercise class
+        this.deleteExercise = this.deleteExercise.bind(this);
+
+        this.state = { exercises: []};
+    }
+
+    componentDidMount()
+    {
+        axios.get('http://localhost:5000/exercises')
+        .then(response => {
+            this.setState({ exercises: response.data})
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    deleteExercise(id) {
+        axios.delete('http://localhost:5000/exercises/' + id)
+        .then(res => console.log(res.data));
+        this.setState({
+            exercises: this.state.exercises.filter( el => el._id !== id) //this updates the exercises state and automatically updates the elements
+            //for every el return if el._id does not equal the id element that's been deleted
+        })
+    }
+
+    exerciseList() {
+        return this.state.exercises.map(currentexercise => {
+            return <Exercise exercise = {currentexercise} deleteExercise = {this.deleteExercise} key = {currentexercise._id} />;
+        })
+    }
+
 
     render() {
         return (
             <div>
-                <p>Exercise List component</p>
+                <h3>Logged Exercises</h3>
+                <table className = "table">
+                    <thead className = "thead-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Description</th>
+                            <th>Duration</th>
+                            <th>Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.exerciseList() }
+                    </tbody>
+                </table>
             </div>
         );
     }
